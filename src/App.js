a7d482f9dd635e7f4dd6652bde3e86900b39aa4d
alexxxstep <astep2004@gmail.com>
2020-11-12 16:57:59 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -64,8 +64,15 @@ class App extends Component {
         <h1>{this.state.pageTitle}</h1>
 
         <button onClick={this.toggleCarsHandler}>Toggle cars</button>
-
-        {cars}
+        <div
+          style={{
+            width: 400,
+            margin: 'auto',
+            paddingTop: '20px',
+          }}
+        >
+          {cars}
+        </div>
       </div>
     )
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,19 +1,45 @@
 import React from 'react'
+import Radium from 'radium'
+import './Car.css'
 
-export default (props) => (
-  <div
-    style={{
-      border: '2px solid #ccc',
-      marginBottom: '10px',
-      display: 'block',
-      padding: '10px',
-    }}
-  >
-    <h3>Сar name: {props.name}</h3>
-    <p>
-      Year: <strong>{props.year}</strong>
-    </p>
-    <input type="text" onChange={props.onChangeName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
-)
+const Car = (props) => {
+  const inputClasses = ['input']
+
+  if (props.name !== '') {
+    inputClasses.push('green')
+  } else {
+    inputClasses.push('red')
+  }
+
+  if (props.name.length > 4) {
+    inputClasses.push('bold')
+  }
+
+  const style = {
+    border: '1px solid #ccc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
+    ':hover': {
+      border: '1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, 0.25)',
+      cursor: 'pointer',
+    },
+  }
+
+  return (
+    <div className="Car" style={style}>
+      <h3>Сar name: {props.name}</h3>
+      <p>
+        Year: <strong>{props.year}</strong>
+      </p>
+      <input
+        className={inputClasses.join(' ')}
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  )
+}
+
+export default Radium(Car)
